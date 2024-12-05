91cbf9318e311ee774c599e035b36efa270d8345
lejelly <jeong@weblab.t.u-tokyo.ac.jp>
2024-12-05 23:09:10 +0900

    final

--- run.sh
+++ run.sh
@@ -3,13 +3,13 @@
 rm ngrok.log
 rm npm_dev.log
 
-nohup ngrok http 3000 > logs/ngrok.log 2>&1 & 
+nohup ngrok http 3000 > logs/ngrok.log & 
 #ngrok.logの Forwarding https://xxxxx.ngrok-free.app をslack APIにコピー
 # 1. Event SubscriptionsのRequest URL
 # 2. Interactivity & ShortcutsのRequest URL
 # 3. Slash CommandsのRequest URL
 
-nohup npm run dev > logs/npm_dev.log 2>&1 &
+nohup npm run dev > logs/npm_dev.log &
 
 
 # 実行中のプロセス確認
